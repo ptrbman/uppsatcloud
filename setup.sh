9a9a35f754fefbfd4ba7853d829b3ccdda731e84
ptrbman <peter@backeman.se>
2018-10-30 15:50:54 +0100

    Minor changes, it should work ok now

--- setup.sh
+++ setup.sh
@@ -1,26 +1,32 @@
 echo "Installing docker"
-sudo apt-get --force-yes update
-sudo apt-get --force-yes  install \
+sudo apt-get -y update
+sudo apt-get -y  install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository --force-yes  \
+sudo add-apt-repository -y  \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get  --force-yes update
-sudo apt-get  --force-yes install docker-ce
+sudo apt-get  -y update
+sudo apt-get  -y install docker-ce
 
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
 
+exec su -l ptr
 
 sudo docker run hello-world
 sudo docker login -u backeman -p uppsat
 sudo docker pull backeman/uppsat:z3
 
-#sudo docker run -v /home/root/benchmarks:/benchmarks /benchmarks/...
+sudo apt -y install python3-venv
+sudo apt -y install python3-pip
 
+python3 -m venv pydocker
+source pydocker/bin/activate
 pip install docker
+
+#sudo docker run -v /home/root/benchmarks:/benchmarks /benchmarks/...
